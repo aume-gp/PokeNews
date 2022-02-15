27c0a78ce92cbe8f6cef578f5ab06830f2ca9586
Yopelait <98877578+Yopelait@users.noreply.github.com>
2022-02-15 23:13:02 +0100

    Mise à jour du fichier JS Gabriel

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 $(window).on("load",function(){
         $(".loader-container").fadeOut(1000);
     });
+
+// Script barre collante en haut de l'écran
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("barrealerte");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
